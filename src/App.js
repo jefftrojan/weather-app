d257cc95f17671ef21b905c90014968c352bc8f6
Jeff Trojan <j.dauda@alustudent.com>
2023-02-03 01:30:33 +0200

    templated app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const api = {
+  key: "69461ea432de4e9adda482f185121ae0",
+  base: "https://api.openweathermap.org/data/2.5/"
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+          <div className="search-box">
+            <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+
+
+            
+            >
+            </input>
+          </div>
+      </main>
+      
     </div>
   );
 }
